bbe41032e48f7d3d52f9816ffd8a3726e6bf3166
Eric <erickohler1600@gmail.com>
2022-04-05 20:32:39 -0400

    adding news section slider

---
+++ assets/js/script.js
@@ -0,0 +1,61 @@
+function getApi() {
+    // TODO: Insert the API url to get a list of your repos
+    var requestUrl = 'https://newsapi.org/v2/top-headlines?q=raptors&country=ca&category=sports&apiKey=8d807af9c2974fe880ecce7b3be08106';
+
+    fetch(requestUrl)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data.articles);
+            for (let index = 0; index < data.articles.length; index++){
+                var newsCol = '';
+                if(data.articles[index].urlToImage != null){
+                    newsCol = '<section style="background-image: url('+data.articles[index].urlToImage+');">';
+                }else{
+                    newsCol = '<section>';
+                }
+
+                newsCol += `                    
+                    <div class="news-content">
+                        <h3>`+data.articles[index].title+`</h3>
+                    <div>
+                    </section>
+                `;
+                
+                $("#news-articles-section").append(newsCol);
+            }
+
+            buildSlider();
+        });
+}
+
+$(document).ready(function() {
+    getApi();
+});
+
+function buildSlider(){
+    $('#news-articles-section').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [
+            {
+              breakpoint: 767,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                arrows: false,
+                dots: true,
+                autoplay: true,
+                autoplaySpeed: 2000,
+              }
+            }
+        ]
+    });
+}
